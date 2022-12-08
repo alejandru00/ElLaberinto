4278d3fca46cd590c655ab5d1016e5776b6d7f13
alejandru00 <alejandrolliber2004@gmail.com>
2022-12-08 14:56:05 +0100

    commit

--- laberinto.py
+++ laberinto.py
@@ -1,4 +1,3 @@
-
 laberinto = [
     ['E', 'X', 'X', 'X', 'X'], 
     [' ', 'X', ' ', ' ', ' '],
@@ -8,12 +7,3 @@ laberinto = [
     ]
 
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-print("\n")
-
-def imprimir_laberinto(laberinto):
-    for fila in laberinto:
-        for celda in fila:
-            print(celda, end=' ')
-        print()
-
-imprimir_laberinto(laberinto)
